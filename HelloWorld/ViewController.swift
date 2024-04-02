fcfb59b3afde7f4c9554142bf7c506a3f3d3253b
Zephyr <zephyr@zephyrs-mac-mini.local>
2024-04-02 13:39:29 -0700

    Initilize Project - Creates base project files - Adds Hello World display label

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Zephyr on 3/22/24.
+//  Copyright © 2024 Cyberfox Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
